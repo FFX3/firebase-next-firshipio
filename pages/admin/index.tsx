ab407fdc210eddb3e63a91a47fec58a9db5d835b
Justin McIntyre <justinmcintyre42@gmail.com>
2022-08-20 17:45:01 -0300

    added auth check component

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,6 +1,13 @@
+import AuthCheck from "../../components/AuthCheck";
+import Metatags from "../../components/Metatags";
+
 export default function Admin({ }) {
 	return (
 		<main>
+			<AuthCheck>
+				<Metatags title="admin page" />
+				<h1>Edit Post</h1>
+			</AuthCheck>
 		</main>
 	)
 }
